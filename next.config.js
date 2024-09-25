93f146caec53a2c02d8af21cd343bf33e7b8ab6b
itsspedram <pedmsv@gmail.com>
2024-09-25 21:52:25 +0330

    chane the project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export', // Enables static export
+    reactStrictMode: true,
+     basePath: '/CV',  // Optional: Set this if you're deploying to a subdirectory
+}
 
 module.exports = nextConfig
